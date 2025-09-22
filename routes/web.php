838582440c18f78a507b3620866c7d75a5848c19
lindy <lindy24si@mahasiswa.pcr.ac.id>
2025-09-22 08:58:23 +0700

    Baru Belajar

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,11 @@ Route::get('/nama/{param1}', function ($param1) {
     return 'Nama saya: '.$param1;
 });
 
+
 Route::get('/nim/{param1?}', function ($param1 = '') {
     return 'NIM saya: '.$param1;
 });
+
 Route::get('/mahasiswa/{param1?}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');;
+
 
